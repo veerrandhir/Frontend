9bd57284091935ffa9412e962528107ff47c6a86
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-08 13:59:06 +0530

    tailwind and its props

---
+++ Tutorial/03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+  <>
+  <h1 className='bg-green-400'>Tailwind Css using</h1>
+  </>
+  )
+}
+
+export default App
