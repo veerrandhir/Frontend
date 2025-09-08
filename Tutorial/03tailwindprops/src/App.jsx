f4aba773129699c3bed18b4672996ba40b84af56
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-08 16:53:38 +0530

    Fix count state management in App component

--- Tutorial/03tailwindprops/src/App.jsx
+++ Tutorial/03tailwindprops/src/App.jsx
@@ -3,11 +3,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  let [count , setCount] = useState(5)
   
+  if(count > 1){
+    count +=1
+  }
+  setCount(count)
 
   return (
   <>
   <h1 className='bg-green-400'>Tailwind Css using</h1>
+  <h2>{count}</h2>
   </>
   )
 }
