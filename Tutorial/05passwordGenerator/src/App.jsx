651441fe091b425ba89d6d19a26c00fe56211aaa
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-10 15:31:59 +0530

    Refactor App component:  initialize state variables

--- Tutorial/05passwordGenerator/src/App.jsx
+++ Tutorial/05passwordGenerator/src/App.jsx
@@ -1,10 +1,15 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [length , setLenght ] = useState(8)   // declreration of all variable components which generarte code for us
   
+  const [numberAllowed , setNumberAllowed]= useState(false)
+  
+  const [charAllowed, setCharAllowed] = useState(false)
+
+  const [password , setPassword] = useState("") // it require string so given empty string here
+
 
   return (
     <>
