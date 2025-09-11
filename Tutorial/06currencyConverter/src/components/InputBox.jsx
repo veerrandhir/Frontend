3ab8c2be7686d3bb9ab95cf0eca19e0eb4c0ce81
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-11 18:24:37 +0530

    component created InputBox

--- Tutorial/06currencyConverter/src/components/InputBox.jsx
+++ Tutorial/06currencyConverter/src/components/InputBox.jsx
@@ -33,9 +33,13 @@ function InputBox({
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
           value={selectCurrency}
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-          disabled= {currencyDisable}
+          disabled={currencyDisable}
         >
-          <option value="usd">usd</option>
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
         </select>
       </div>
     </div>
