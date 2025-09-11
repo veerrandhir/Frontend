6001e5003bfb306150deb9909da60901ea0629aa
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-11 18:35:38 +0530

    useId

---
+++ Tutorial/06currencyConverter/src/components/index.js
@@ -0,0 +1,6 @@
+// Best way to import any component is to directly import all them in a single file then from here use
+// It reduce code redundency
+
+import InputBox from "./InputBox";
+
+export { InputBox };
