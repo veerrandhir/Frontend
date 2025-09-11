ded49987387dddc1128509e1728f55aa74e13474
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-11 18:48:42 +0530

    currencyInfo , swap, options created

--- Tutorial/06currencyConverter/src/App.jsx
+++ Tutorial/06currencyConverter/src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks";
+import "./App.css";
+import { fromJSON } from "postcss";
 
-import './App.css'
+
+
 
 function App() {
+  
+  const [amount, setAmount] = useState(0)
+  
+  const [from , setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
 
+  const [convertedAmount , setConvertedAmount] = useState(0)
 
-  return (
-   <h1 className='text-3xl bg-orange-500'> Currency Converter App</h1>
-  )
+  const currencyInfo =  useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  // swep to variables
+
+  const swap = ()=>{
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+
+  }
+
+  return <h1 className="text-3xl bg-orange-500"> Currency Converter App</h1>;
 }
 
-export default App
+export default App;
--- Tutorial/06currencyConverter/src/components/InputBox.jsx
+++ Tutorial/06currencyConverter/src/components/InputBox.jsx
@@ -12,14 +12,18 @@ function InputBox({
 
   className = "",
 }) {
-    const amountInputId = useId()
+  const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
       <div className="w-1/2">
-        <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
         <input
-        
-        id = {amountInputId}
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
