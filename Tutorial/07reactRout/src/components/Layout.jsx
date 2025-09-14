8ecb10c57dc0107c188fb87f55a82f0bbb12b3d8
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-14 19:00:36 +0530

    library updated for Outlet

--- Tutorial/07reactRout/src/components/Layout.jsx
+++ Tutorial/07reactRout/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
-import {Outlet} from 'react-dom'
+import {Outlet} from 'react-router-dom'
 function Layout() {
   return (
     <>
