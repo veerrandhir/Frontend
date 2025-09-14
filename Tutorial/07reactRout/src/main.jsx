52190bcc244d09bb11791429f1a570d91faa981b
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-14 18:59:56 +0530

    alternate way and common way to router

--- Tutorial/07reactRout/src/main.jsx
+++ Tutorial/07reactRout/src/main.jsx
@@ -1,28 +1,44 @@
-import { StrictMode } from "react";
 import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Layout from "./components/Layout.jsx";
 import { Children } from "react";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import User from './components/User/User.jsx'
 
-const router = createBrowserRouter([
-  { path: "/", 
-    element: <Layout />,
-    Children: [{
-      path: '',
-      element :<Home/>
+// const router = createBrowserRouter([
+//   { path: "/", 
+//     element: <Layout />,
+//     Children: [{
+//       path: '',
+//       element :<Home/>
 
-    },
-  {
-    path:'about',
-    element: <About/>
-  }] 
-  },
-]);
+//     },
+//   {
+//     path:'about',
+//     element: <About/>
+//   }] 
+//   },
+// ]);
+// Alternative way to define route
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element= {<Layout/>}>
+      <Route path="" element ={< Home/>}/>
+      <Route path="contact" element ={< Contact/>}/>
+      <Route path="about" element ={< About/>}/>
+      <Route path="user/:userid" element ={< User/>}/>
+    </Route>
+  )
+)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
