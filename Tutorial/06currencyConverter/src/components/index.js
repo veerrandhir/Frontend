b70c16a9b61d7b0e0960511d184c5cedea4a77ac
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-14 15:30:01 +0530

    updated index and useCurrency

--- Tutorial/06currencyConverter/src/components/index.js
+++ Tutorial/06currencyConverter/src/components/index.js
@@ -1,6 +1,6 @@
 // Best way to import any component is to directly import all them in a single file then from here use
 // It reduce code redundency
 
-import InputBox from "./InputBox";
+import InputBox from "./InputBox.jsx";
 
 export { InputBox };
--- Tutorial/06currencyConverter/src/hooks/useCurrencyInfo.js
+++ Tutorial/06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,20 +1,17 @@
-import { data } from "autoprefixer";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-  // useEffect always takse 1 call back fn and a dependendency when it see any change in dependency it call fn
-
   const [data, setData] = useState({});
   useEffect(() => {
     fetch(
-      //Gives us chaining so we can use .then as much
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
       .then((res) => res.json())
-      .then((res) => setData(res[currency])); // we call api and this custom hook will give us a result
+      .then((res) => setData(res[currency]));
     console.log(data);
   }, [currency]);
-  return data; // we directly return data from this custom hook
+  console.log(data);
+  return data;
 }
 
 export default useCurrencyInfo;
