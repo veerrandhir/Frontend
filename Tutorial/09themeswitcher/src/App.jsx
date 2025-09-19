8d72737acdfbbea47260d58ef1fd656472d081d5
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-19 20:17:46 +0530

    value on themProvider added

--- Tutorial/09themeswitcher/src/App.jsx
+++ Tutorial/09themeswitcher/src/App.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./context/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  {
+    /* Actual change in a theme done by Js */
+  }
+
+  useEffect(() => {
+    document.querySelector("html").className.remove("light", "dark ");
+  }, [themeMode]);
   return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-        {/* Theme button */}
-        <div className="w-full max-w-sm mx-auto">{/* Theme button */}</div>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          {/* Theme button */}
+          <div className="w-full max-w-sm mx-auto">{/* Theme button */}</div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
