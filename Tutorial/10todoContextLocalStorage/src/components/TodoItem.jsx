ea03cd0fbf44eaa3be4748c15f937aa00de11f71
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-20 19:20:53 +0530

    todoItem fn defined

--- Tutorial/10todoContextLocalStorage/src/components/TodoItem.jsx
+++ Tutorial/10todoContextLocalStorage/src/components/TodoItem.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { useState } from "react";
+import { useTodo } from "../contexts";
 
 function TodoItem({ todo }) {
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
+
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
+
+  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+
+    setIsTodoEditable(false);
+  };
+
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
+
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
