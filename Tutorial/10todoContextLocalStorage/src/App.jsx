5ffd83171bd6e4681e5f8fc3e242baaeaa458e1b
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-20 17:57:28 +0530

    Spacing

--- Tutorial/10todoContextLocalStorage/src/App.jsx
+++ Tutorial/10todoContextLocalStorage/src/App.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
+
 import "./App.css";
+
 import { TodoProvider } from "./contexts";
 
 function App() {
   const [todos, setTodos] = useState([]);
+
   const addTodo = (todo) => {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
+
   const deleteTodo = () => {};
+
   const updateTodo = (id, todo) => {
     setTodos((prev) =>
       prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
     );
   };
+
   const toggleComplete = () => {};
+
   return (
     <TodoProvider
       value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete }}
@@ -23,7 +30,9 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
+
           <div className="mb-4">{/* Todo form goes here */}</div>
+
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
           </div>
