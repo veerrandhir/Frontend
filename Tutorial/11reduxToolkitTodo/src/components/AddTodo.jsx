5c6ef0342501a9505c78c1f31a4f420282f75cca
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-22 18:44:40 +0530

    Tailwind added

--- Tutorial/11reduxToolkitTodo/src/components/AddTodo.jsx
+++ Tutorial/11reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function AddTodo() {
-  return <div>AddTodo</div>;
+  return (
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      <input
+        type="text"
+        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-color duration-200 ease-in-out"
+        placeholder="Enter a todo..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <buttom />
+    </form>
+  );
 }
 
 export default AddTodo;
