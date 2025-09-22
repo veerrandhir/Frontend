254a88dd836ab160545c6d8f1e4b16d0da7f1877
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-22 20:27:44 +0530

    Debugging

--- Tutorial/11reduxToolkitTodo/src/components/AddTodo.jsx
+++ Tutorial/11reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import useDispatch from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { addTodo } from "../features/todo/todoSlice";
 
@@ -24,7 +24,12 @@ function AddTodo() {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <buttom />
+      <buttom
+        type="submit"
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >
+        Add Todo
+      </buttom>
     </form>
   );
 }
