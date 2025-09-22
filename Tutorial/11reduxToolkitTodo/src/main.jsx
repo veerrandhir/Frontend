2d8e45e60b733c68df82e39626c2ae4d437fdf89
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-22 20:27:33 +0530

    debugging

--- Tutorial/11reduxToolkitTodo/src/main.jsx
+++ Tutorial/11reduxToolkitTodo/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
 
-createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <App />
   </Provider>
