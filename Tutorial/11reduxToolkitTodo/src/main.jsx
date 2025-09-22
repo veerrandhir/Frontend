619fefba76f3fdca706f2af19363f9675abfffc8
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-22 20:10:38 +0530

    provider on main file to wrap

--- Tutorial/11reduxToolkitTodo/src/main.jsx
+++ Tutorial/11reduxToolkitTodo/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
