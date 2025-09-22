90da21ae822278ec911d56ff85762efd75c8ed6a
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-22 20:07:32 +0530

    User todos components into app

--- Tutorial/11reduxToolkitTodo/src/App.jsx
+++ Tutorial/11reduxToolkitTodo/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
       <h1>Learn about Redux tool</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
