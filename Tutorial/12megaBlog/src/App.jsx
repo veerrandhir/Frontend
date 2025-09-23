6eaf29fb6aa54a4fb806635550f86d3ffc88fc59
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-23 20:01:14 +0530

    conf for env file

--- Tutorial/12megaBlog/src/App.jsx
+++ Tutorial/12megaBlog/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL); // "123"
+  // console.log(import.meta.env.DB_PASSWORD); // undefined
   return (
     <>
       <h1>Mega blog Project</h1>
