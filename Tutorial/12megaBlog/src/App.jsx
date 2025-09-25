98f5ead9379aa3b88046b23ecf1168724de6c382
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-25 21:02:32 +0530

    Import useEffect and minor effects

--- Tutorial/12megaBlog/src/App.jsx
+++ Tutorial/12megaBlog/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 
+import { useEffect } from "react";
+
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 
