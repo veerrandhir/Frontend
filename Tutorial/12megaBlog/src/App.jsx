83d44a39b13b4fa54e04d5ce273e0687e38fc4cb
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-25 20:32:50 +0530

    footer defined

--- Tutorial/12megaBlog/src/App.jsx
+++ Tutorial/12megaBlog/src/App.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL); // "123"
+  console.log("App is using env file", import.meta.env.VITE_APPWRITE_URL); // "123"
   // console.log(import.meta.env.DB_PASSWORD); // undefined
-  return (
-    <>
-      <h1>Mega blog Project</h1>
-    </>
-  );
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false)); // in place of catch we had used finally to setLoading false so that loading icon won't show after any other page open
+  }, []); // dependency array
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>// TODO {/*<Outlet/> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
