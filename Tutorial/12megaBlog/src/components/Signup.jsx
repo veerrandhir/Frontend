8a67253561cec174c5691ff178d8792bb37d34ce
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-26 19:39:02 +0530

    Get user data form authservice

--- Tutorial/12megaBlog/src/components/Signup.jsx
+++ Tutorial/12megaBlog/src/components/Signup.jsx
@@ -10,6 +10,29 @@ import { useDispatch } from "react-redux";
 import { useform } from "react-hook-form";
 
 function Signup() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useform();
+
+  const create = async (data) => {
+    setError("");
+
+    if (userData) {
+      const userData = await authService.createAccount(data);
+
+      try {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(login(userData));
+
+        navigate("/");
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+  };
+
   return <div>Signup</div>;
 }
 
