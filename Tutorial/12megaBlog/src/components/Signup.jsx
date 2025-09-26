53914384b8f57faad86d66ae2f6c2a1b8b8b9670
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-26 19:30:00 +0530

    import depends

--- Tutorial/12megaBlog/src/components/Signup.jsx
+++ Tutorial/12megaBlog/src/components/Signup.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import authService from "../appwrite/auth";
+
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../store/authSlice";
+
+import { Button, Input, Logo } from "./index";
+import { useDispatch } from "react-redux";
+
+import { useform } from "react-hook-form";
 
 function Signup() {
   return <div>Signup</div>;
