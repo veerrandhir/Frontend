4afd896bb3b0707473acdfc3b7ddc278ef258dc9
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 17:49:34 +0530

    Implement post fetching logic in EditPost component based on slug

--- Tutorial/12megaBlog/src/pages/EditPost.jsx
+++ Tutorial/12megaBlog/src/pages/EditPost.jsx
@@ -8,6 +8,18 @@ function EditPost() {
   const [posts, setPosts] = useState(null);
   const { slug } = useParams();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPosts(post);
+        } else {
+          navigate("/");
+        }
+      });
+    }
+  });
   return <div>EditPost</div>;
 }
 
