058164d2f4ce3c43e8c708ed351d1687253b2f96
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 16:33:47 +0530

    Add missing imports and exports for Logo, LogoutBtn, RTE, and Signup components

--- Tutorial/12megaBlog/src/components/index.js
+++ Tutorial/12megaBlog/src/components/index.js
@@ -1,4 +1,13 @@
 import Header from "./Header/Header.jsx";
 
 import Footer from "./Footer/Footer.jsx";
-export { Header, Footer };
+import Logo from "./Logo.jsx";
+
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+
+import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
+
+import Login from "../pages/Login.jsx";
+
+export { Header, Footer, Logo, LogoutBtn, RTE, Signup, Login };
