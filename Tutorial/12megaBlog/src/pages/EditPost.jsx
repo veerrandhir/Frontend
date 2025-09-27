33662a6f5d087f8ad07dfd25afb3e4e875ea2bfe
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 17:45:44 +0530

    Add EditPost component with initial implementation

---
+++ Tutorial/12megaBlog/src/pages/EditPost.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+import { Container, PostForm } from "../components";
+import appwriteService from "../appwrite/config";
+
+function EditPost() {
+  const [posts, setPosts] = useState(null);
+  return <div>EditPost</div>;
+}
+
+export default EditPost;
