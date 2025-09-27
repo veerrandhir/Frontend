33052948a3ba26d0c1f7015cc0ee3cb0a39198e9
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 18:15:44 +0530

    Implement Post component with fetching and deletion logic

--- Tutorial/12megaBlog/src/pages/Post.jsx
+++ Tutorial/12megaBlog/src/pages/Post.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config";
+import { Button, Container } from "../components";
+import parse from "html-react-parser";
+import { useSelector } from "react-redux";
 
-function Post() {
-  return <div>Post</div>;
-}
+export default function Post() {
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-export default Post;
+  const userData = useSelector((state) => state.auth.userData);
+
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) setPost(post);
+        else navigate("/");
+      });
+    } else navigate("/");
+  }, [slug, navigate]);
+
+  const deletePost = () => {
+    appwriteService.deletePost(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredImage);
+        navigate("/");
+      }
+    });
+  };
