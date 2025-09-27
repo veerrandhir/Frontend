050e618f6e9ff9d2e5a80816df1af3f86b58d2a4
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 16:35:54 +0530

    Fix import statement for Login component in Login.jsx

--- Tutorial/12megaBlog/src/pages/Login.jsx
+++ Tutorial/12megaBlog/src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Loginas loginComponent } from "../components";
+import { Login as loginComponent } from "../components";
 
 function Login() {
-  return (<div className="py-8">
-    <loginComponent/>
-  </div>
-  )
+  return (
+    <div className="py-8">
+      <loginComponent />
+    </div>
+  );
 }
 
 export default Login;
