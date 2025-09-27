989a3353abf7c4abe4088cbb0c30c8cb8408d2e0
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 18:09:49 +0530

    Implement post fetching logic in Home component

--- Tutorial/12megaBlog/src/pages/Home.jsx
+++ Tutorial/12megaBlog/src/pages/Home.jsx
@@ -4,6 +4,16 @@ import appwriteService from "../appwrite/config";
 import { Container, PostCard } from "../components";
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+
   return <div>Home</div>;
 }
 
