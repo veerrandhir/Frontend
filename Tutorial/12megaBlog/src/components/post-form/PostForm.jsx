fab339b61ac3912b9a1f4bdcea226d9ec7456aff
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 16:03:53 +0530

    Refactor PostForm component to include form setup and default values

--- Tutorial/12megaBlog/src/components/post-form/PostForm.jsx
+++ Tutorial/12megaBlog/src/components/post-form/PostForm.jsx
@@ -1,6 +1,24 @@
 import React, { useCallback } from "react";
 import { useForm } from "react-hook-form";
+
+
 import { Button, Input, RTE, Select } from "..";
 import appwriteService from "../../appwrite/config";
+
+
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+
+
+export default function PostForm({ post }) {
+    const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
+        defaultValues: {
+            title: post?.title || "",
+            slug: post?.$id || "",
+            content: post?.content || "",
+            status: post?.status || "active",
+        },
+    });
+
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.auth.userData);
