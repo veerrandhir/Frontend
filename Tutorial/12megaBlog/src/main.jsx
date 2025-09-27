388fb4bc75f66266234740d3f6e08c4611aaff09
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 18:30:15 +0530

    Add routing configuration for signup, all-posts, add-post, edit-post, and post components

--- Tutorial/12megaBlog/src/main.jsx
+++ Tutorial/12megaBlog/src/main.jsx
@@ -26,6 +26,45 @@ const router = createBrowserRouter([
           </AuthLayout>
         ),
       },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
     ],
   },
 ]);
