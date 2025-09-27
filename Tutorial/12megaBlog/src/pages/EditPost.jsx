d4a49db20bcefdecba2ecb868a80601485f1cd97
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 17:46:37 +0530

    Add navigation and parameter handling in EditPost component

--- Tutorial/12megaBlog/src/pages/EditPost.jsx
+++ Tutorial/12megaBlog/src/pages/EditPost.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 
 import { Container, PostForm } from "../components";
 import appwriteService from "../appwrite/config";
+import { useNavigate, useParams } from "react-router-dom";
 
 function EditPost() {
   const [posts, setPosts] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
   return <div>EditPost</div>;
 }
 
