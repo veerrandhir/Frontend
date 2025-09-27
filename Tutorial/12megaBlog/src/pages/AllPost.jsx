a2a3fa4b172fbe74be7d9da76b9d3e51d433433e
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 17:07:30 +0530

    Add AllPost component with initial implementation

---
+++ Tutorial/12megaBlog/src/pages/AllPost.jsx
@@ -0,0 +1,13 @@
+{
+  /* To show all post in a page */
+}
+
+import React, { useState, useEffect } from "react";
+import { Container, PostCard } from "../components/index.js";
+import appwriteService from "../appwrite/config.js";
+
+function AllPost() {
+  return <div>AllPost</div>;
+}
+
+export default AllPost;
