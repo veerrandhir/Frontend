8fdadbe8e7de4ed07bb0028a8093de1005f6b8b0
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 18:27:57 +0530

    Refactor routing configuration to include AuthLayout for login component

--- Tutorial/12megaBlog/src/main.jsx
+++ Tutorial/12megaBlog/src/main.jsx
@@ -7,24 +7,28 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { createBrowserRouter } from "react-router-dom";
-
+import { AuthLayout } from "./components/index.js";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element:<App/>,
-    children:[{
-      path: '/',
-      element:<Home/>,
-    },
-    {
-      path:"/login",
-      element: ()
-    }
-
-    ]
-  }
-])
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
