50860947e6b889d48416ca01b0027b025497e14f
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-27 17:18:43 +0530

    Render posts in AllPost component using Container and PostCard components

--- Tutorial/12megaBlog/src/pages/AllPost.jsx
+++ Tutorial/12megaBlog/src/pages/AllPost.jsx
@@ -17,7 +17,19 @@ function AllPost() {
     }
   });
 
-  return <div>AllPost</div>;
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard post={post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default AllPost;
