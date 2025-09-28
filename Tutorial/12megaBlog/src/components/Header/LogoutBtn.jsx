3a3c2ac6cffc69211f21658176d04652aa8b2885
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-28 13:45:17 +0530

    Debug import path at logoutbtn

--- Tutorial/12megaBlog/src/components/Header/LogoutBtn.jsx
+++ Tutorial/12megaBlog/src/components/Header/LogoutBtn.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
-import { logout } from "../../store/authSlice";
+// import { logout } from "../../appwrite/auth";
+
+const logout = authService.logout;
 
 function LogoutBtn() {
   const dispatch = useDispatch();
