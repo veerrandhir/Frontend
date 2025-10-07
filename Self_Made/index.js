85d41bf1e5e5beae5a98a36926a28d82040216ff
Randhir Kumar <veerrandhir3@gmail.com>
2025-10-07 15:25:44 +0530

    feat: implement basic HTTP server setup in index.js

--- Self_Made/index.js
+++ Self_Made/index.js
@@ -1,1 +1,27 @@
 // Self made project on which I have to work
+
+
+// let's create a http server
+
+const https =  require "https"
+
+
+const createServer = https.createServer((req , res)=> console.log('server started')
+)
+
+createServer.listen(4000, ()=>{
+    console.log("server is serving");
+    
+
+})
+
+req.url(
+    switch (url) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+)
